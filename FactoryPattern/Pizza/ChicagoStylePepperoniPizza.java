0b059a97a4343bc32e7451c63235f2944e2506f7
philFernandez <norcal618@gmail.com>
2021-11-11 14:57:26 -0800

    add Chicago

---
+++ FactoryPattern/Pizza/ChicagoStylePepperoniPizza.java
@@ -0,0 +1,28 @@
+
+public class ChicagoStylePepperoniPizza extends Pizza {
+
+    @Override
+    public void prepare() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void bake() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void cut() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void box() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
