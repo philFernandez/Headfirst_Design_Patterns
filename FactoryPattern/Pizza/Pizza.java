8983ea11ce1953c653441cf0316eda57764939b8
philFernandez <norcal618@gmail.com>
2021-11-11 16:22:53 -0800

    Pizza.java book code

--- FactoryPattern/Pizza/Pizza.java
+++ FactoryPattern/Pizza/Pizza.java
@@ -1,9 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Pizza {
-    public abstract void prepare();
+    String name;
+    String dough;
+    String sauce;
+    List<String> toppings = new ArrayList<>();
 
-    public abstract void bake();
+    void prepare() {
+        System.out.println("Preparing " + name);
+        System.out.println("Tossing dough...");
+        System.out.println("Adding Sauce...");
+        System.out.println("Adding Toppings:");
 
-    public abstract void cut();
+        for (String topping : toppings) {
+            System.out.println("   " + topping);
+        }
+    }
 
-    public abstract void box();
+    void bake() {
+        System.out.println("Bake for 25 minutes at 350");
+    }
+
+    void cut() {
+        System.out.println("Cutting the pizza into diagonal slices");
+    }
+
+    void box() {
+        System.out.println("Place pizza into official pizza store box");
+    }
+
+    public String getName() {
+        return name;
+    }
 }
