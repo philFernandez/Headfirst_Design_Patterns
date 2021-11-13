a2944b34ab80e5e4d2466db21fd1d9d3900d70fa
philFernandez <norcal618@gmail.com>
2021-11-13 00:56:00 -0800

    RemoteLoader (Driver code)

---
+++ ComandPattern/remote/RemoteLoader.java
@@ -0,0 +1,39 @@
+
+public class RemoteLoader {
+    public static void main(String[] args) {
+        RemoteControl remoteControl = new RemoteControl();
+        Light livingRoomLight = new Light("Living Room");
+        Light kitchenLight = new Light("Kitchen Light");
+        CeilingFan ceilingFan = new CeilingFan("Living Room");
+        GarageDoor garageDoor = new GarageDoor("Garage");
+        Stereo stereo = new Stereo("Living Room");
+
+        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
+        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
+
+        CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
+        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+
+        GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+        GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
+
+        StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+
+        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+        remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+        remoteControl.setCommand(2, ceilingFanOn, ceilingFanOff);
+        remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
+        remoteControl.setCommand(4, garageDoorUp, garageDoorDown);
+
+        System.out.println(remoteControl);
+
+        for (var i = 0; i < 5; i++) {
+            remoteControl.onButtonWasPushed(i);
+            remoteControl.offButtonWasPushed(i);
+        }
+
+    }
+}
