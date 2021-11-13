b84c1ed19118d572d5783cd424649ab23ed9149d
philFernandez <norcal618@gmail.com>
2021-11-12 16:53:18 -0800

    Garage and GarageOpenCommand commit

---
+++ ComandPattern/simpleremote/GarageDoorOpenCommand.java
@@ -0,0 +1,15 @@
+public class GarageDoorOpenCommand implements Command {
+
+    GarageDoor garageDoor;
+
+    public GarageDoorOpenCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    @Override
+    public void execute() {
+        garageDoor.up();
+        // garageDoor.lightOn(); // it didn't ask for this
+    }
+
+}
