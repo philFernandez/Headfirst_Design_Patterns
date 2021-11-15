794a7307d45928fd043b80a506ca447cd5678afd
philFernandez <norcal618@gmail.com>
2021-11-15 00:13:51 -0800

    CeilingFanCommands (with undo)

--- ComandPattern/remote/CeilingFanLowCommand.java
+++ ComandPattern/remote/CeilingFanLowCommand.java
@@ -1,16 +1,16 @@
-public class CeilingFanOffCommand implements Command {
+public class CeilingFanLowCommand implements Command {
 
     CeilingFan ceilingFan;
     int prevSpeed;
 
-    public CeilingFanOffCommand(CeilingFan ceilingFan) {
+    public CeilingFanLowCommand(CeilingFan ceilingFan) {
         this.ceilingFan = ceilingFan;
     }
 
     @Override
     public void execute() {
         prevSpeed = ceilingFan.getSpeed();
-        ceilingFan.off();
+        ceilingFan.low();
     }
 
     @Override
--- ComandPattern/remote/CeilingFanMediumCommand.java
+++ ComandPattern/remote/CeilingFanMediumCommand.java
@@ -1,16 +1,16 @@
-public class CeilingFanOffCommand implements Command {
+public class CeilingFanMediumCommand implements Command {
 
     CeilingFan ceilingFan;
     int prevSpeed;
 
-    public CeilingFanOffCommand(CeilingFan ceilingFan) {
+    public CeilingFanMediumCommand(CeilingFan ceilingFan) {
         this.ceilingFan = ceilingFan;
     }
 
     @Override
     public void execute() {
         prevSpeed = ceilingFan.getSpeed();
-        ceilingFan.off();
+        ceilingFan.medium();
     }
 
     @Override
