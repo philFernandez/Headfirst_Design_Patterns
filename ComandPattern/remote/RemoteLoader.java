bb89f7ab1c0f16388e8328b8dae58ed1d2bae6dc
philFernandez <norcal618@gmail.com>
2021-11-15 21:45:18 -0800

    CeilingFanCommands (with undo) Test

--- ComandPattern/remote/RemoteLoader.java
+++ ComandPattern/remote/RemoteLoader.java
@@ -2,20 +2,23 @@
 public class RemoteLoader {
     public static void main(String[] args) {
         RemoteControl remoteControl = new RemoteControl();
-        Light livingRoomLight = new Light("Living Room");
-        LightOnCommand lightOn = new LightOnCommand(livingRoomLight);
-        LightOffCommand lightOff = new LightOffCommand(livingRoomLight);
 
-        remoteControl.setCommand(0, lightOn, lightOff);
+        CeilingFan ceilingFan = new CeilingFan("Living Room");
+
+        CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
+        CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
+        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+
+        remoteControl.setCommand(0, ceilingFanMedium, ceilingFanOff);
+        remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff);
 
         remoteControl.onButtonWasPushed(0);
         remoteControl.offButtonWasPushed(0);
         System.out.println(remoteControl);
         remoteControl.undoButtonWasPushed();
-        remoteControl.offButtonWasPushed(0);
-        remoteControl.onButtonWasPushed(0);
+
+        remoteControl.onButtonWasPushed(1);
         System.out.println(remoteControl);
         remoteControl.undoButtonWasPushed();
-
     }
 }
