68c87d3fec206409a333ca02295c5ed3ac838f0c
philFernandez <norcal618@gmail.com>
2021-11-15 22:02:26 -0800

    Macro Command

---
+++ ComandPattern/remote/MacroCommand.java
@@ -0,0 +1,24 @@
+
+public class MacroCommand implements Command {
+
+    Command[] commands;
+
+    public MacroCommand(Command[] commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    public void execute() {
+        for (var i = 0; i < commands.length; i++) {
+            commands[i].execute();
+        }
+    }
+
+    @Override
+    public void undo() {
+        for (var i = 0; i < commands.length; i++) {
+            commands[i].undo();
+        }
+    }
+
+}
--- ComandPattern/remote/RemoteLoader.java
+++ ComandPattern/remote/RemoteLoader.java
@@ -4,21 +4,37 @@ public class RemoteLoader {
         RemoteControl remoteControl = new RemoteControl();
 
         CeilingFan ceilingFan = new CeilingFan("Living Room");
+        Light light = new Light("Living Room");
+        Stereo stereo = new Stereo("Living Room");
+        GarageDoor garageDoor = new GarageDoor("Garage");
 
-        CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
         CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
-        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+        LightOnCommand lightOn = new LightOnCommand(light);
+        StereoOnWithCDCommand stereoOn = new StereoOnWithCDCommand(stereo);
+        GarageDoorUpCommand garageUp = new GarageDoorUpCommand(garageDoor);
 
-        remoteControl.setCommand(0, ceilingFanMedium, ceilingFanOff);
-        remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff);
+        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+        LightOffCommand lightOff = new LightOffCommand(light);
+        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+        GarageDoorDownCommand garageDown = new GarageDoorDownCommand(garageDoor);
+
+        Command[] partyOn = { ceilingFanHigh, lightOn, stereoOn, garageUp };
+        Command[] partyOff = { ceilingFanOff, lightOff, stereoOff, garageDown };
+
+        MacroCommand partyOnMacro = new MacroCommand(partyOn);
+        MacroCommand partyOffMacro = new MacroCommand(partyOff);
+
+        remoteControl.setCommand(0, partyOnMacro, partyOffMacro);
+        System.out.println(remoteControl);
 
         remoteControl.onButtonWasPushed(0);
+        System.out.println(remoteControl);
+        remoteControl.undoButtonWasPushed();
+        System.out.println(remoteControl);
+        remoteControl.onButtonWasPushed(0);
+        System.out.println(remoteControl);
         remoteControl.offButtonWasPushed(0);
         System.out.println(remoteControl);
-        remoteControl.undoButtonWasPushed();
 
-        remoteControl.onButtonWasPushed(1);
-        System.out.println(remoteControl);
-        remoteControl.undoButtonWasPushed();
     }
 }
