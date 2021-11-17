48d76b60c4f910c1c5a0460aecb3bc39cb5ca26c
philFernandez <norcal618@gmail.com>
2021-11-16 21:04:02 -0800

    DuckAdapter

--- AdapterPattern/ducks/DuckTestDrive.java
+++ AdapterPattern/ducks/DuckTestDrive.java
@@ -3,19 +3,36 @@ public class DuckTestDrive {
         Duck duck = new MallardDuck();
         Turkey turkey = new WildTurkey();
         Duck turkeyAdapter = new TurkeyAdapter(turkey);
+        Turkey duckAdapter = new DuckAdapter(duck);
 
         System.out.println("The turkey says...");
-        turkey.gobble();
-        turkey.fly();
+        testTurkey(turkey);
+        sep();
 
         System.out.println("The duck says...");
         testDuck(duck);
+        sep();
+
         System.out.println("The turkeyAdapter says...");
         testDuck(turkeyAdapter);
+        sep();
+
+        System.out.println("The duckAdapter says...");
+        testTurkey(duckAdapter);
+        sep();
     }
 
     public static void testDuck(Duck duck) {
         duck.quack();
         duck.fly();
+    }
+
+    public static void testTurkey(Turkey turkey) {
+        turkey.gobble();
+        turkey.fly();
+    }
+
+    public static void sep() {
+        System.out.println("--------------------");
     }
 }
