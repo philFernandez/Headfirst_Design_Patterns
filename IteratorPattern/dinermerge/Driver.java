04f957c3c7d84470c3c58fad32acc49108631d2b
philFernandez <norcal618@gmail.com>
2021-11-23 00:39:53 -0800

    java waitress in separate file

--- IteratorPattern/dinermerge/Driver.java
+++ IteratorPattern/dinermerge/Driver.java
@@ -1,24 +1,7 @@
 
 public class Driver {
     public static void main(String[] args) {
-        var pancakeHouseMenu = new PancakeHouseMenu();
-        var<MenuItem> breakfastItems = pancakeHouseMenu.getMenuItems();
-
-        var dinerMenu = new DinerMenu();
-        MenuItem[] lunchItems = dinerMenu.getMenuItems();
-
-        for (var i = 0; i < breakfastItems.size(); i++) {
-            var menuItem = breakfastItems.get(i);
-            System.out.println(menuItem.getName() + " ");
-            System.out.println(menuItem.getPrice() + " ");
-            System.out.println(menuItem.getDescription());
-        }
-
-        for (var i = 0; i < lunchItems.length; i++) {
-            var menuItem = lunchItems[i];
-            System.out.println(menuItem.getName() + " ");
-            System.out.println(menuItem.getPrice() + " ");
-            System.out.println(menuItem.getDescription());
-        }
+        JavaEnabledWaitress waitress = new JavaEnabledWaitress();
+        waitress.printMenu();
     }
 }
---
+++ IteratorPattern/dinermerge/JavaEnabledWaitress.java
@@ -0,0 +1,23 @@
+public class JavaEnabledWaitress {
+    public void printMenu() {
+        var pancakeHouseMenu = new PancakeHouseMenu();
+        var<MenuItem> breakfastItems = pancakeHouseMenu.getMenuItems();
+
+        var dinerMenu = new DinerMenu();
+        MenuItem[] lunchItems = dinerMenu.getMenuItems();
+
+        for (var i = 0; i < breakfastItems.size(); i++) {
+            var menuItem = breakfastItems.get(i);
+            System.out.println(menuItem.getName() + " ");
+            System.out.println(menuItem.getPrice() + " ");
+            System.out.println(menuItem.getDescription());
+        }
+
+        for (var i = 0; i < lunchItems.length; i++) {
+            var menuItem = lunchItems[i];
+            System.out.println(menuItem.getName() + " ");
+            System.out.println(menuItem.getPrice() + " ");
+            System.out.println(menuItem.getDescription());
+        }
+    }
+}
